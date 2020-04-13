346dd615c6209d45772879db3a713126e8aaf83d
wangxinquan <wangxinquan@anji-tec.com>
2020-04-13 13:08:48 +0800

    @SpringBootApplication详解

--- spring-boot-01-helloworld/src/main/java/com/tec/anji/HelloWorldMainApplication.java
+++ spring-boot-01-helloworld/src/main/java/com/tec/anji/HelloWorldMainApplication.java
@@ -5,6 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Administrator
+ * 1. SpringBootConfiguration
+ *  1.1 Configuration === Component
+ * 2. EnableAutoConfiguration
+ *  2.1 AutoConfigurationPackage
+ *   2.1.1 @Import(AutoConfigurationPackages.Registrar.class)
+ *    1）扫描注解@SpringBootApplication类所在包(com.tec.anji)下的所有配置文件
+ *  2.2 @Import(EnableAutoConfigurationImportSelector.class)
+ *   2.2.1 AutoConfigurationImportSelector.selectImports(...)
+ *    1) SpringFactoriesLoader.loadFactoryNames(...)
+ *    2) 从spring-boot-autoconfigure.META-INF/spring.factories获取EnableAutoConfiguration下的所有自动配置类
  */
 @SpringBootApplication
 public class HelloWorldMainApplication {
