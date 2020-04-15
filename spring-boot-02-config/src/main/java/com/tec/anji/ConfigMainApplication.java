754cadeb6887fc5da3bbda51171da05ee8ac7824
wangxinquan <825877689@qq.com>
2020-04-15 21:03:17 +0800

    自动配置原理

--- spring-boot-02-config/src/main/java/com/tec/anji/ConfigMainApplication.java
+++ spring-boot-02-config/src/main/java/com/tec/anji/ConfigMainApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 /**
- * @ImportResource：描述自定义bean配置文件路径
+ * 1. @ImportResource：描述自定义bean配置文件路径
+ * 2. 自动配置
+ *  2.1 自定义XXXXAutoConfiguration
+ *  2.2 类注解
+ *      2.2.1 @Configuration
+ *      2.2.2 @EnableConfigurationProperties(XXXXProperties-@ConfigurationProperties)
+ *      2.2.3 构造注入(无需@Autowired注解)
+ *  2.3 [配置参考](https://docs.spring.io/spring-boot/docs/2.1.13.RELEASE/reference/html/common-application-properties.html)
  */
 //@ImportResource({"classpath:bean.xml"})
 @SpringBootApplication
