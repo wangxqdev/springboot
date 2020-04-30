1f992411d2b9de686b0ca0891b47ce4abf0524ba
wangxinquan <wangxinquan@anji-tec.com>
2020-04-30 16:32:10 +0800

    引入国际化

--- spring-boot-04-web/src/main/java/com/tec/anji/config/MyMvcConfig.java
+++ spring-boot-04-web/src/main/java/com/tec/anji/config/MyMvcConfig.java
@@ -1,7 +1,9 @@
 package com.tec.anji.config;
 
+import com.tec.anji.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -23,5 +25,10 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
                 registry.addViewController("/index").setViewName("login");
             }
         };
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
     }
 }
