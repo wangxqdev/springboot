2324028acdd7c408a4d894ccdf5cac0a6838ff49
wangxinquan <wangxinquan@anji-tec.com>
2020-05-01 15:45:09 +0800

    提取公共模板

--- spring-boot-04-web/src/main/java/com/tec/anji/controller/EmployeeController.java
+++ spring-boot-04-web/src/main/java/com/tec/anji/controller/EmployeeController.java
@@ -1,13 +1,24 @@
 package com.tec.anji.controller;
 
+import com.tec.anji.dao.EmployeeDao;
+import com.tec.anji.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeDao employeeDao;
+
     @GetMapping("/emps")
-    public String list() {
+    public String list(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps", employees);
         return "emp/list";
     }
 }
