7b6313715c553584cf2dcfd7096605ca48aabbe9
wangxinquan <825877689@qq.com>
2020-05-01 22:42:51 +0800

    员工添加

--- spring-boot-04-web/src/main/java/com/tec/anji/controller/EmployeeController.java
+++ spring-boot-04-web/src/main/java/com/tec/anji/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.tec.anji.controller;
 
+import com.tec.anji.dao.DepartmentDao;
 import com.tec.anji.dao.EmployeeDao;
+import com.tec.anji.entities.Department;
 import com.tec.anji.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -15,10 +19,26 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
