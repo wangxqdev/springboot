e49d712d55b9f51235a09f762c85790870d4dccd
wangxinquan <825877689@qq.com>
2020-05-01 23:18:23 +0800

    员工修改

--- spring-boot-04-web/src/main/java/com/tec/anji/controller/EmployeeController.java
+++ spring-boot-04-web/src/main/java/com/tec/anji/controller/EmployeeController.java
@@ -4,15 +4,18 @@ import com.tec.anji.dao.DepartmentDao;
 import com.tec.anji.dao.EmployeeDao;
 import com.tec.anji.entities.Department;
 import com.tec.anji.entities.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
+@Slf4j
 @Controller
 public class EmployeeController {
 
@@ -38,6 +41,24 @@ public class EmployeeController {
 
     @PostMapping("/emp")
     public String addEmp(Employee employee) {
+        log.info("Create employee = {}", employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable int id, Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee) {
+        log.info("Modify employee = {}", employee);
         employeeDao.save(employee);
         return "redirect:/emps";
     }
